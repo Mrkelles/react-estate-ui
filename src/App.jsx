bddbf33259803af3747ceef98255edef4c73f2ef
Kelvin Ebima <kelvinebima@gmail.com>
2024-07-01 01:22:26 -0700

    implement navbar and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";import "./layout.scss"
+import HomePage from "./routes/homepage/homepage";
+import Layout from "./routes/layout/layout";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        // {
+        //   path:"/list",
+        //   element:<ListPage/>
+        // },
+        // {
+        //   path:"/:id",
+        //   element:<SinglePage/>
+        // },
+        // {
+        //   path:"/profile",
+        //   element:<ProfilePage/>
+        // },
+        // {
+        //   path:"/login",
+        //   element:<Login/>
+        // },
+        // {
+        //   path:"/register",
+        //   element:<Register/>
+        // }
+      ]
+    }
+  ]);
+
   return (
-    <div>Hello World</div>
-  )
+
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
